590177ee154d928ecefa55ecbf9b9403efbe5765
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-28 21:35:01 +0400

    Optimized image loading

--- rick-and-morty-app/Extensions/UIImageView.swift
+++ rick-and-morty-app/Extensions/UIImageView.swift
@@ -14,17 +14,18 @@ extension UIImageView {
     func getImageFromUrl(imageUrl: String) {
         guard let url = URL(string: imageUrl) else { return }
         
-        DispatchQueue.main.async { [weak self] in
+        if let cacheData = CacheManager.getImageCache(url) {
+            self.image = cacheData
+            return
+        }
+        
+        DispatchQueue.global().async { [weak self] in
             if let imageData = try? Data(contentsOf: url) {
-                
-                if let cacheData = CacheManager.getImageCache(url) {
-                    self?.image = cacheData
-                    return
-                }
-                
                 if let loadedImage = UIImage(data: imageData) {
-                    CacheManager.setImageCache(url, loadedImage)
-                    self?.image = loadedImage
+                    DispatchQueue.main.async {
+                        self?.image = loadedImage
+                        CacheManager.setImageCache(url, loadedImage)
+                    }
                 }
             }
         }
